e4660cc246abd7c8975f6a8707150e58e8386826
Chinmaytikole <123376493+Chinmaytikole@users.noreply.github.com>
2025-08-19 16:15:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'MINIMALSITE'))
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
